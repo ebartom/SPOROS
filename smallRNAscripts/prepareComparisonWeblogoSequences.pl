9bf27c4bbe9dc73828de3b43e844dae45871d6ab
Elizabeth Bartom <ebartom@northwestern.edu>
2021-05-31 04:34:09 -0500

    Added scripts for processing differential seeds.

---
+++ smallRNAscripts/prepareComparisonWeblogoSequences.pl
@@ -0,0 +1,59 @@
+#!/opt/local/bin/perl -w
+
+use strict;
+
+if($#ARGV < 0) {
+  print STDERR "Useage: $0 < comparisonsFile > [project] \n";
+  exit;
+}
+
+my $compfile = $ARGV[0];
+my $project = $ARGV[1];
+if ($project){ $project = ".$project";} else {$project = "";}
+
+my @labels;
+my @data;
+my %samples;
+
+open (IN,$compfile);
+while (<IN>){
+    chomp $_;
+#    print "line: $_\n";
+    if ($_ =~ /^Comparison/){
+	@labels = split(/\,/,$_);
+	for (my $i=0;$i<=$#labels;$i++){
+	    $samples{$i} = $labels[$i];
+#	    print "$labels[$i]\n";
+	}
+    } else {
+	@data = split(/\,/,$_);
+	my $comparison = $data[0];
+	my @group1samples;
+	my @group2samples;
+	for (my $i=1;$i<=$#data;$i++){
+	    my $sample = $samples{$i};
+	    if ($data[$i] == 1){
+		push (@group1samples,$sample);
+	    } elsif ($data[$i] == -1){
+		push (@group2samples,$sample);
+	    }
+	}
+	print "$comparison\n@group1samples\n@group2samples\n";
+	foreach my $direction ("up","dn") {
+	    my $outfile1 = "seedAnalysis.$comparison".$project.".group1.$direction.txt";
+	    my $outfile2 = "seedAnalysis.$comparison".$project.".group2.$direction.txt";
+	    system("touch $outfile1");
+	    system("touch $outfile2");
+	    foreach my $sample (@group1samples){
+		my $syscommand = "cat seedAnalysis.$sample*$comparison*$direction.1000.txt >> $outfile1";
+		print "$syscommand\n";
+		system($syscommand);
+	    }	    
+	    foreach my $sample (@group2samples){
+		my $syscommand = "cat seedAnalysis.$sample*$comparison*$direction.1000.txt >> $outfile2";
+		print "$syscommand\n";
+		system($syscommand);
+	    }
+	}
+    }
+}
---
+++ smallRNAscripts/thresholdDifferential.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use File::Basename;
+use List::Util qw( min max );
+
+my $edgeRfile          = $ARGV[0];
+my $thresholdParameter = $ARGV[1];
+my $logFCthresh        = $ARGV[2];
+my $statThresh         = $ARGV[3];
+
+if ($#ARGV <0){
+    die "Usage:  $0 <file> <parameter> <logFCthresth> <statThresh>\n";
+}
+
+if ($thresholdParameter eq "adjp"){
+    $thresholdParameter = "adj.p";
+}
+if (($thresholdParameter eq "PValue") || ($thresholdParameter eq "pvalue")){
+    $thresholdParameter = "PValue";
+}
+
+my $iStat = 0;
+my $iLogFC = 0;
+
+my $prefix = $edgeRfile;
+if ($edgeRfile =~ /^([\-\.\w\d]+).txt$/){
+    $prefix = $1;
+}
+
+open(UP,">$prefix.up.txt");
+open(DN,">$prefix.dn.txt");
+
+open(IN,$edgeRfile);
+while(<IN>){
+    chomp $_;
+    if (($_ =~ /^Read/) ){
+	print "$_\n";
+	my @labels = split(/\t/,$_);
+	for(my $i=0;$i<=$#labels;$i++){
+	    if($labels[$i] eq $thresholdParameter){
+		$iStat = $i;
+	    }
+	    if ($labels[$i] eq "logFC"){
+		$iLogFC = $i;
+		print "Column for LogFC: $iLogFC\n";
+	    }
+	}
+	print UP "$labels[0]";
+	print DN "$labels[0]";
+	for (my $i=1;$i<$iLogFC;$i++){
+	    print UP "\t$labels[$i]";
+	    print DN "\t$labels[$i]";
+	}
+#        print UP "\tNormTotal";
+#        print DN "\tNormTotal";
+	print UP "\t$labels[-2]";
+	print UP "\t$labels[-1]\n";
+	print DN "\t$labels[-2]";
+	print DN "\t$labels[-1]\n";
+    } else {
+	my @data = split(/\t/,$_);
+	#	my @countsData = @data[0..$logFC-1];
+	my $stat = $data[$iStat];
+	if ($stat <= $statThresh){
+	    my $logFC = $data[$iLogFC];
+#	    print "$stat\t$statThresh\t$logFC\t$logFCthresh\t$thresholdParameter\n";
+	    if ($logFC <= (-1*$logFCthresh)){
+		print DN "$data[0]";
+		for (my $j=1;$j<$iLogFC;$j++){
+		    print DN "\t$data[$j]";
+		}
+#	 	print DN "\t0";
+		print DN "\t$data[-2]";
+		print DN "\t$data[-1]";
+		print DN "\n";
+	    } elsif ($logFC >= $logFCthresh){
+		print UP "$data[0]";
+		for (my $j=1;$j<$iLogFC;$j++){
+		    print UP "\t$data[$j]";
+		}
+#		print UP "\t0";
+		print UP "\t$data[-2]";
+		print UP "\t$data[-1]";
+		print UP "\n";
+	    }
+	}
+    }
+}
+
+	    
