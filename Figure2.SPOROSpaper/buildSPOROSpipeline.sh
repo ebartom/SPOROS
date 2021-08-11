8aaa55f69cb3af44f14b370cfdaea7a834ed9ef9
Elizabeth Bartom <ebartom@northwestern.edu>
2021-08-10 19:38:56 -0500

    SPOROSv1.0 pipeline to accompany paper submission.

--- Figure2.SPOROSpaper/buildSPOROSpipeline.sh
+++ Figure2.SPOROSpaper/buildSPOROSpipeline.sh
@@ -1,5 +1,7 @@
-perl /projects/b1069/buildSPOROSpipeline.pl -o human -id Figure2 -w /projects/b1069/Figure2.SPOROSpaper \
-	-sequenceType sRNA \
+perl /projects/b1069/buildSPOROSpipeline.pl \
+	-o human \
+	-id Figure2 \
+	-w /projects/b1069/Figure2.SPOROSpaper \
 	-f /projects/b1069/Figure2.SPOROSpaper/input/fastq/ \
 	-b /projects/b1069/Figure2.SPOROSpaper/input/HCT116_Drosha_Dicer_rep1.barcodes.txt,/projects/b1069/Figure2.SPOROSpaper/input/HCT116_Drosha_Dicer_rep2.barcodes.txt 
 
